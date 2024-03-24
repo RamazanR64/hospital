7fe7dc3c3e0a312408a62b6be0ff8f6e1bfc0bfb
RamazanR64 <160380364+RamazanR64@users.noreply.github.com>
2024-03-24 20:03:54 +0500

    improve

--- api/mixins.py
+++ api/mixins.py
@@ -1,12 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
 
-from .permissions import RoleBasedPermissionMixin, HasPermissionByAuthenticatedUserRole
+class HasPermissionByAuthenticatedUserRole(permissions.BasePermission):
+    # Add your custom permission logic here
+    pass
 
+class CustomPagination(PageNumberPagination):
+    # Configure your custom pagination settings here
+    pass
 
-class HospitalGenericViewSet(
-    RoleBasedPermissionMixin,
-    viewsets.GenericViewSet,
-):
+class HospitalGenericViewSet(viewsets.GenericViewSet):
     permission_classes = [HasPermissionByAuthenticatedUserRole]
+    pagination_class = CustomPagination
 
-    # pagination_class = [CustomPagination, ]
+    def get_queryset(self):
+        # Add your custom queryset logic here
+        pass
+
+    def list(self, request, *args, **kwargs):
+        # Add your custom list view logic here
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        # Add your custom retrieve view logic here
+        pass
+
+    # Add other action methods as needed
