0b248e5721e6660fb668c94fcf49273148b76ab3
RamazanR64 <160380364+RamazanR64@users.noreply.github.com>
2024-03-24 20:03:56 +0500

    improve

--- api/urls.py
+++ api/urls.py
@@ -1,87 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView)
-
+from rest_framework.routers import DefaultRouter
 from .views.patient import PatientViewSet
 from .views.doctor import DoctorView, ServiceViewSet, VisitViewSet
 
-urlpatterns = [
-    path(
-        'doctor/',
-        DoctorView.as_view({
-            'get': 'list',
-            'post': 'create'
-        })
-    ),
+router = DefaultRouter()
+router.register(r'patient', PatientViewSet, basename='patient')
+router.register(r'service', ServiceViewSet, basename='service')
+router.register(r'visit', VisitViewSet, basename='visit')
+router.register(r'doctor', DoctorView, basename='doctor')
 
-    path(
-        'doctor/<int:id>',
-        DoctorView.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })
-    ),
-    path(
-        'doctor/<int:id>/patient',
-        DoctorView.as_view({
-            'get': 'list_patient',
-        })
-    ),
-    path(
-        'patient/',
-        PatientViewSet.as_view({
-            'get': 'list',
-            'post': 'create'
-        })
-    ),
-    path(
-        'patient/<int:id>',
-        PatientViewSet.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })
-    ),
-    path(
-        'token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
-    path(
-        'token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path(
-        'service/',
-        ServiceViewSet.as_view({
-            'get': 'list',
-            'post': 'create'
-        })
-    ),
-    path(
-        'service/<int:id>',
-        ServiceViewSet.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })
-    ),
-    path(
-        'visit/',
-        VisitViewSet.as_view({
-            'get': 'list',
-            'post': 'create'
-        })
-    ),
-    path(
-        'visit/<int:id>',
-        VisitViewSet.as_view({
-            'get': 'retrieve',
-            'put': 'update',
-            'delete': 'destroy'
-        })
-    ),
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include(router.urls)),
+    path('doctor/<int:doctor_id>/patient/', DoctorView.list_patient, name='doctor_patient_list'),
 ]
