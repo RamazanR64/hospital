dc11e466bc939300bbc52eda9e7da290485f42f7
RamazanR64 <160380364+RamazanR64@users.noreply.github.com>
2024-03-24 20:03:50 +0500

    improve

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
 class ApiConfig(AppConfig):
+    """Configuration for the 'api' app."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
